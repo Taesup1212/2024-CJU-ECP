bf66c1cbdfcae0ecb145c91b47590d3ec5522b7b
TAESUP <1230ts@naver.com>
2024-10-15 16:55:52 +0900

    241015_ECP_Calculator

---
+++ 241015_ECP/241015_ECP.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void) {
+    char n; // Specify '+, -, /, *' as the character
+    int a, b;
+
+    printf("Input the formula (ex: 5+5) : ");
+    scanf_s("%d %c %d", &a, &n, 1, &b);
+
+    switch (n) 
+    {
+    case '+':
+        {
+            printf("\n%d", a + b);
+            break;
+        }
+    case '-':
+        {
+            printf("\n%d", a - b);
+            break;
+        }
+    case '/':
+        {
+            printf("\n%d", a / b);
+            break;
+        }
+    case '*':
+        {
+            printf("\n%d", a * b);
+            break;
+        }
+    default:
+        {
+            printf("\nWrong Input");
+            break;
+        }
+    }
+
+    return 0;
+}
+
