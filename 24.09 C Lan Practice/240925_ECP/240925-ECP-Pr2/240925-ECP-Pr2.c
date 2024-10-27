f676e80fe10f4f274e7f70e4bb9d21fc69f445d4
TAESUP <1230ts@naver.com>
2024-10-27 09:32:10 +0900

    .

--- 24.09 C Lan Practice/240925_ECP/240925-ECP-Pr2/240925-ECP-Pr2.c
+++ 24.09 C Lan Practice/240925_ECP/240925-ECP-Pr2/240925-ECP-Pr2.c
@@ -5,8 +5,9 @@ int main(void)
 	double ft, in, slug, m, kg = 0.0;
 
 	printf("Input values : (ft to m, m to ft, in to m, m to in, slug to kg, kg to slug)\n");
-	scanf_s("%lf %lf %lf %lf %lf %lf", &ft, &m, &in, &m, &slug, &kg);
+	scanf("%lf %lf %lf %lf %lf %lf", &ft, &m, &in, &m, &slug, &kg);
 	printf("%.2lfm, %.2lfft, %.2lfm, %.2lfin, %.2lfkg, %.2lfslug", 0.3048 * ft, 3.2808 * m, 0.0254 * in, 39.3701 * m, 14.5939 * slug, 0.0685 * kg);
 
 	return 0;
 }
+
